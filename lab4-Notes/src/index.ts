45ff6544bd3b180a9a6b76b6ffa0a888ee444086
chwnk <48645500+chwnk@users.noreply.github.com>
2021-05-16 11:05:52 +0200

    initial commit

---
+++ lab4-Notes/src/index.ts
@@ -0,0 +1,22 @@
+import './style.scss';
+
+export class Main {
+
+
+    
+    // weatherData: IWeatherData[];
+    // activeCity: IWeatherData['id'] = Number(localStorage.getItem(ACTIVE_CITY_KEY));
+
+    // constructor() {
+        
+    // }
+
+    // changeActiveCity(index: IWeatherData['id']) {
+    //     this.activeCity = index;
+    //     localStorage.setItem(ACTIVE_CITY_KEY, String(index));
+    // }
+
+}
+
+
+new Main();
