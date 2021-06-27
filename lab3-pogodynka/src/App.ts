53e628530c0ac1ece393eee5837c488a3adaad23
chwnk <chowaniak.bartosz@protonmail.com>
2021-06-27 11:20:18 +0200

    Fixed adding cities
    Fixed adding cities

--- lab3-pogodynka/src/App.ts
+++ lab3-pogodynka/src/App.ts
@@ -6,13 +6,14 @@ class Application {
     }
 
     public buttonHandle = () : void => {
+        console.log('buttonHandle');
         const input: HTMLInputElement = document.querySelector('input[id="location"]') as HTMLInputElement;
         if (input != null) {
             const city = input.value;
             
             this.getWeather(city);
-            this.render();
         }
+        setTimeout(() => this.render(), 1000);
     }
 
     // Gets weather forecast for provided city
@@ -25,31 +26,28 @@ class Application {
         .then((response) => {
             const data = response.data;
             this.addToLocalStorage(data);
-            return data;
         })
         .catch((error) => {
             console.log(error);
         })
-        return;
     }
 
     // Saves data to localstorage
-    private setData(data : any) : void {
+    private setData(data: any) : void {
         localStorage.setItem('weatherData', JSON.stringify(data));
     }
 
     // Gets saved data from localstorage
     private getData() : Array<any> {
-        const data : string | null = localStorage.getItem('weatherData');
-        return data ? JSON.parse(data) : []
+        const data: string | null = localStorage.getItem('weatherData');
+        return data ? JSON.parse(data) : [];
     }
 
     // Adds new data to current saved data in localstorage
-    private addToLocalStorage(data : any) : void {
-        let tab : Array<any> = [];
+    private addToLocalStorage(data: {}) : void {
+        let tab: Array<any> = [];
         tab = this.getData();
         tab.push(data);
-
         this.setData(tab);
     }
 
@@ -57,38 +55,37 @@ class Application {
     private render(): void {
         // gets section with weather items
         const cities: HTMLElement | null = document.getElementById('widgets');
+        cities.innerHTML = '';
 
-        if (cities !== null) {
-            let elements = this.getData();
-            elements.forEach(item => {
-                const cityItem: HTMLElement = document.createElement('div');
-                cityItem.classList.add('card');
+        let elements = this.getData();
+        elements.forEach(item => {
+            const cityItem: HTMLElement = document.createElement('div');
+            cityItem.classList.add('card');
 
-                const cityName: HTMLElement = document.createElement('h2.cityName');
-                cityName.classList.add('cityName');
-                cityName.innerHTML = item.name;
+            const cityName: HTMLElement = document.createElement('h2.cityName');
+            cityName.classList.add('cityName');
+            cityName.innerHTML = item.name;
 
-                const description: HTMLElement = document.createElement('p');
-                description.classList.add('description');
-                description.innerHTML = item.weather[0].description;
+            const description: HTMLElement = document.createElement('p');
+            description.classList.add('description');
+            description.innerHTML = item.weather[0].description;
 
-                const temperature: HTMLElement = document.createElement('p');
-                temperature.classList.add('temperature');
-                temperature.innerHTML = `${item.main.temp} <sup>o</sup>C`;
+            const temperature: HTMLElement = document.createElement('p');
+            temperature.classList.add('temperature');
+            temperature.innerHTML = `${item.main.temp} <sup>o</sup>C`;
 
-                const image = document.createElement('img');
-                image.classList.add('weatherIcon');
-                const imgSource: string = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
-                image.src = imgSource;
+            const image = document.createElement('img');
+            image.classList.add('weatherIcon');
+            const imgSource: string = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+            image.src = imgSource;
 
-                cityItem.append(cityName);
-                cityItem.append(image);
-                cityItem.append(temperature);
-                cityItem.append(description);
+            cityItem.append(cityName);
+            cityItem.append(image);
+            cityItem.append(temperature);
+            cityItem.append(description);
 
-                cities.append(cityItem);
-            })
-        }
+            cities.append(cityItem);
+        })
     }
 };
 
